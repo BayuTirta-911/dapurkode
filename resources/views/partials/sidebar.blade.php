5fde39f38ddb890788f2b28a1e75ce28714ba0a6
BayuTirta-911 <bayutovan911@gmail.com>
2025-01-13 09:51:02 +0700

    minor language changes

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -23,7 +23,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('admin.dashboard') }}" aria-expanded="false">
                 <iconify-icon icon="ri:admin-fill"></iconify-icon>
-                    <span class="hide-menu">Admin Dashboard</span>
+                    <span class="hide-menu">Dashboard Admin</span>
                 </a>
             </li>
             @endif
@@ -32,7 +32,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('profile') }}" aria-expanded="false">
                 <iconify-icon icon="mdi:account"></iconify-icon>
-                    <span class="hide-menu">Your Profile</span>
+                    <span class="hide-menu">Profil Kamu</span>
                 </a>
             </li>
             @if (auth()->check() && auth()->user()->role === 'admin')
@@ -40,7 +40,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('admin.profile_manager') }}" aria-expanded="false">
                 <iconify-icon icon="mdi:account-cog"></iconify-icon>
-                    <span class="hide-menu">Profile Manager</span>
+                    <span class="hide-menu">Profil Manager</span>
                 </a>
             </li>
             @endif
@@ -50,7 +50,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('bank_accounts.index') }}" aria-expanded="false">
                     <iconify-icon icon="solar:file-text-line-duotone"></iconify-icon>
-                    <span class="hide-menu">Bank Accounts</span>
+                    <span class="hide-menu">Akun Bank</span>
                 </a>
             </li>
             @endif
@@ -60,7 +60,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('vendor.groups.index') }}" aria-expanded="false">
                     <iconify-icon icon="eos-icons:products-outlined"></iconify-icon>
-                    <span class="hide-menu">Make Product Group</span>
+                    <span class="hide-menu"> Buat Grup Produk</span>
                 </a>
             </li>
             @endif
@@ -70,7 +70,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('vendor.services.index') }}" aria-expanded="false">
                 <iconify-icon icon="hugeicons:new-job"></iconify-icon>
-                    <span class="hide-menu">My Services</span>
+                    <span class="hide-menu">Jasa</span>
                 </a>
             </li>
             @endif
@@ -80,7 +80,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('admin.services.index') }}" aria-expanded="false">
                 <iconify-icon icon="hugeicons:new-job"></iconify-icon>
-                    <span class="hide-menu">Manage Vendor Service</span>
+                    <span class="hide-menu">Kelola Jasa Vendor</span>
                 </a>
             </li>
             @endif
@@ -90,7 +90,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('admin.services.highlight') }}" aria-expanded="false">
                 <iconify-icon icon="line-md:star-filled" width="24" height="24"></iconify-icon>
-                    <span class="hide-menu">Highlighted Service</span>
+                    <span class="hide-menu">Highlight Jasa</span>
                 </a>
             </li>
             @endif
@@ -100,7 +100,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('admin.discounts.index') }}" aria-expanded="false">
                 <iconify-icon icon="ic:outline-discount"></iconify-icon>
-                    <span class="hide-menu">Manage Discount</span>
+                    <span class="hide-menu">Kelola Diskon</span>
                 </a>
             </li>
             @endif
@@ -110,7 +110,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('admin.purchases.index') }}" aria-expanded="false">
                 <iconify-icon icon="mdi:invoice-text-edit-outline"></iconify-icon>
-                    <span class="hide-menu">Manage Invoices</span>
+                    <span class="hide-menu">Kelola Invoice</span>
                 </a>
             </li>
             @endif
@@ -120,7 +120,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('affiliate.request.create') }}" aria-expanded="false">
                 <iconify-icon icon="fluent-mdl2:team-favorite"></iconify-icon>
-                    <span class="hide-menu">Request Affiliate</span>
+                    <span class="hide-menu">Permintaan Affiliate</span>
                 </a>
             </li>
             @endif
@@ -130,7 +130,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('affiliate.purchases.index') }}" aria-expanded="false">
                 <iconify-icon icon="tabler:affiliate" width="24" height="24"></iconify-icon>
-                    <span class="hide-menu">See Affiliated Purchases</span>
+                    <span class="hide-menu">Pembelian Ter-affiliated</span>
                 </a>
             </li>
             @endif
@@ -140,7 +140,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('affiliate.services.index') }}" aria-expanded="false">
                 <iconify-icon icon="fluent-mdl2:product-variant" width="24" height="24"></iconify-icon>
-                    <span class="hide-menu">See Affiliated Services</span>
+                    <span class="hide-menu">Jasa Ter-Affiliasi</span>
                 </a>
             </li>
             @endif
@@ -150,7 +150,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('affiliate.balance') }}" aria-expanded="false">
                 <iconify-icon icon="mdi:gift-outline" width="24" height="24"></iconify-icon>
-                    <span class="hide-menu">See Your Rewards</span>
+                    <span class="hide-menu">Klaim Saldo</span>
                 </a>
             </li>
             @endif
@@ -160,7 +160,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('installer.balance') }}" aria-expanded="false">
                 <iconify-icon icon="mdi:gift-outline" width="24" height="24"></iconify-icon>
-                    <span class="hide-menu">See Your Balance</span>
+                    <span class="hide-menu">Lihat Saldo</span>
                 </a>
             </li>
             @endif
@@ -170,7 +170,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('vendor.balance') }}" aria-expanded="false">
                 <iconify-icon icon="mdi:gift-outline" width="24" height="24"></iconify-icon>
-                    <span class="hide-menu">See Your Balance</span>
+                    <span class="hide-menu">Lihat Saldo</span>
                 </a>
             </li>
             @endif
@@ -180,7 +180,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('withdraw.index') }}" aria-expanded="false">
                 <iconify-icon icon="uil:money-withdraw" width="24" height="24"></iconify-icon>
-                    <span class="hide-menu">Request Withdrawal</span>
+                    <span class="hide-menu">Request Penarikan</span>
                 </a>
             </li>
             @endif
@@ -191,7 +191,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('admin.withdraws.index') }}" aria-expanded="false">
                 <iconify-icon icon="solar:hand-money-broken" width="24" height="24"></iconify-icon>
-                    <span class="hide-menu">Withdrawal Requests</span>
+                    <span class="hide-menu">Request Penarikan</span>
                 </a>
             </li>
             @endif
@@ -201,7 +201,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('admin.affiliate.requests.index') }}" aria-expanded="false">
                 <iconify-icon icon="fluent-mdl2:team-favorite"></iconify-icon>
-                    <span class="hide-menu">Manage Request Affiliate</span>
+                    <span class="hide-menu">Kelola Request Penarikan</span>
                 </a>
             </li>
             @endif
@@ -211,7 +211,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('admin.project_requests.index') }}" aria-expanded="false">
                 <iconify-icon icon="ix:project" width="24" height="24"></iconify-icon>
-                    <span class="hide-menu">Manage Request Project</span>
+                    <span class="hide-menu">Kelola Request Project</span>
                 </a>
             </li>
             @endif
@@ -221,7 +221,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('admin.projects.index') }}" aria-expanded="false">
                 <iconify-icon icon="ph:cube-focus" width="24" height="24"></iconify-icon>
-                    <span class="hide-menu">Manage Projects</span>
+                    <span class="hide-menu">Kelola Project</span>
                 </a>
             </li>
             @endif
@@ -231,7 +231,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('project.requests.index') }}" aria-expanded="false">
                 <iconify-icon icon="ix:project" width="24" height="24"></iconify-icon>
-                    <span class="hide-menu">Find a Project</span>
+                    <span class="hide-menu">Cari Project</span>
                 </a>
             </li>
             @endif
@@ -250,7 +250,7 @@
             <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ route('user.invoices.index') }}" aria-expanded="false">
                 <iconify-icon icon="tdesign:shop"></iconify-icon>
-                    <span class="hide-menu">My Purchases</span>
+                    <span class="hide-menu">Keranjang Pembelian</span>
                 </a>
             </li>
             
